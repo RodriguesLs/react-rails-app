69f48584cc0ab82e28552d5f462d046f671500fd
Rodrigues <fr.costa95@gmail.com>
2021-01-26 23:49:45 -0300

    feat(function): include delete funciont on component list (=

--- todo-react-app/src/components/tasks/list.component.jsx
+++ todo-react-app/src/components/tasks/list.component.jsx
@@ -1,36 +1,44 @@
 import React from 'react'
-import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 import Table from 'react-bootstrap/Table'
 import { FaCheckCircle, FaTrashAlt } from 'react-icons/fa'
 
-export const ListComponent = ({ tasks }) => (
-  <div>
-    <Card>
-      <Card.Body>
-        <Table responsive>
-          <tbody>
-            {tasks.map((task, i) => {
-              return <tr key={task.id}>
-                <td className="col-md-10">{task.title}</td>
-                <td>
-                  {
-                    task.done == false ? 
-                    <a className="check" href="#">
-                      <FaCheckCircle />
-                    </a> : null
-                  }
-                </td>
-                <td>
-                  <a className="delete" href="#">
-                    <FaTrashAlt />
-                  </a>
-                </td>
-              </tr>
-            })}
-          </tbody>
-        </Table>
-      </Card.Body>
-    </Card>
-  </div>
-)
+const deleteTask = async (task, loadTasks) => {
+  if (window.confirm(`Are you sure you want to delete: "${task.title}"`)) {
+    await fetch(`http://localhost:3001/tasks/${task.id}`, {method: 'DELETE'});
+    loadTasks();
+  }
+}
+
+export const ListComponent = ({ tasks, loadTasks }) => {
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <Table responsive>
+            <tbody>
+              {tasks.map((task, i) => {
+                return <tr key={task.id}>
+                  <td className="col-md-10">{task.title}</td>
+                  <td>
+                    {
+                      task.done == false ? 
+                      <a className="check" href="#">
+                        <FaCheckCircle />
+                      </a> : null
+                    }
+                  </td>
+                  <td>
+                    <a className="delete" href="#" onClick={() => deleteTask(task, loadTasks)}>
+                      <FaTrashAlt />
+                    </a>
+                  </td>
+                </tr>
+              })}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
--- todo-react-app/src/components/tasks/tasks.component.jsx
+++ todo-react-app/src/components/tasks/tasks.component.jsx
@@ -28,11 +28,11 @@ export class TasksComponent extends React.Component {
         <Row>
           <Col xs={{ span: 8, offset: 2}} className="tasks-list">
             <p className="title">To-do</p>
-            <ListComponent tasks={this.state.tasks.filter(task => task.done != true )} />
+            <ListComponent loadTasks={this.loadTasks} tasks={this.state.tasks.filter(task => task.done != true )} />
           </Col>
           <Col xs={{ span: 8, offset: 2 }} className="tasks-list">
             <p className="title">Done</p>
-            <ListComponent tasks={this.state.tasks.filter(task => task.done == true )}/>
+            <ListComponent loadTasks={this.loadTasks} tasks={this.state.tasks.filter(task => task.done == true )}/>
           </Col>
         </Row>
       </div>
